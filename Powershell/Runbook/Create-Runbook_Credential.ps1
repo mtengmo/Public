d3768ef53ce686219088c81a8f05a4ac1edbc4cd
Magnus Tengmo <mteo@tobii.com>
2019-09-11 16:30:46 +0200

    update

--- Powershell/Runbook/Create-Runbook_Credential.ps1
+++ Powershell/Runbook/Create-Runbook_Credential.ps1
@@ -1,4 +1,8 @@
 Connect-AzureRmAccount
+
+#$cred = Get-Credential
+
+
 $user = "user"
 $pw = ConvertTo-SecureString "secretpassword" -AsPlainText -Force
 $cred = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $user, $pw
@@ -6,3 +10,9 @@ $rsg = "rsg"
 $accountname = "accountname" 
 $credentialname = "cred name"
 New-AzureRmAutomationCredential -AutomationAccountName $accountname -Name $credentialname -Value $cred -ResourceGroupName $rsg
+
+
+$User = "Contoso\DChew"
+$Password = ConvertTo-SecureString "Password" -AsPlainText -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $Password
+Set-AzureRmAutomationCredential -AutomationAccountName $accountname -Name $credentialname -ResourceGroupName $rsg -Value $cred
