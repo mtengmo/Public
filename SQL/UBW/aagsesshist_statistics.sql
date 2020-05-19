9f74dbf0df7bbb88fe8d1d20de3c9886338f837a
Magnus Tengmo (Ext) <ext.magnus.tengmo@tieto.com>
2020-05-19 14:30:56 +0200

    unique

--- SQL/UBW/aagsesshist_statistics.sql
+++ SQL/UBW/aagsesshist_statistics.sql
@@ -1,7 +1,10 @@
+use db_u_monserver
+
 -- aggreate session statistics from Unit4 BusinessWorld (Agresso) per day, used in monserver webinfo solution
 -- performance trends
--- Magnus Tengmo (magnus@highcloud.se) - 2020-05-15
--- status 
+-- Magnus Tengmo (magnus@highcloud.se) - 2020-05-20
+-- added unique
+--
 -- U = Unknown user
 -- C Illegal combination of user and client
 -- S Check user status
@@ -11,6 +14,7 @@
 -- E Not allowed to logon at this time (too early)
 -- L Not allowed to logon at this time (too late)
 
+
 /*
 --drop table agresso_aagsesshist_stats
 create table agresso_aagsesshist_stats
@@ -19,7 +23,8 @@ login_date date,
 system_name char(8),
 client varchar(25),
 status char(1),
-counts int
+counts int,
+counts_unique int
 )
 create unique clustered  index ixcu1_agresso_aagsesshist_stats on agresso_aagsesshist_stats (
 login_date asc,client asc,system_name asc,status asc)
@@ -32,6 +37,7 @@ insert into agresso_aagsesshist_stats
 , client
 , status 
 , count(*) as counts
+, count(distinct(user_id)) as counts_unique
     from db_p_agresso.dbo.aagsesshist
     where login_time > convert(date,getdate()-1)
         and login_time < convert(date,getdate())
@@ -43,9 +49,11 @@ union all
 , '_Alla' as client
 , status 
 , count(*) as counts
+, count(distinct(user_id)) as counts_unique
     from db_p_agresso.dbo.aagsesshist
     where login_time > convert(date,getdate()-1)
         and login_time < convert(date,getdate())
     group by convert(date,login_time), system_name, status
 --order by login_date desc
+
 
