b4abf4c97224000ad432fb8049e41824a2004acd
Magnus Tengmo (Ext) <ext.magnus.tengmo@tieto.com>
2020-07-17 09:41:00 +0200

    table

--- SQL/Freediskspace.sql
+++ SQL/Freediskspace.sql
@@ -1,4 +1,19 @@
-SELECT DISTINCT @@servername, dovs.logical_volume_name AS LogicalName,
+/*
+create table diskfreespace (
+servername varchar(255),
+checkdate datetime,
+LogicalName varchar(255),
+Drive varchar(255),
+FreespaceinMb bigint,
+Total_bytesInMB bigint
+)
+
+create unique clustered  index ixcu1_diskfreespace on diskfreespace (servername, checkdate,logicalname)
+*/
+insert into diskfreespace
+SELECT DISTINCT @@servername as servername
+, GETDATE() as checkdate
+, dovs.logical_volume_name AS LogicalName,
     dovs.volume_mount_point AS Drive,
     CONVERT(INT,dovs.available_bytes/1048576.0) AS FreeSpaceInMB,
     CONVERT(INT,dovs.total_bytes/1048576.0) AS total_bytesInMB
@@ -6,3 +21,4 @@ FROM sys.master_files mf
 CROSS APPLY sys.dm_os_volume_stats(mf.database_id, mf.FILE_ID) dovs
 ORDER BY FreeSpaceInMB ASC
 GO
+
