b5dacc3171dbcd28e495773dcc7a3272e0ab56ae
Magnus Tengmo <magnus.tengmo@tobii.com>
2022-03-07 11:21:30 +0100

    mfa details

--- Powershell/Azure MFA/mfa_details.ps1
+++ Powershell/Azure MFA/mfa_details.ps1
@@ -1,3 +1,6 @@
+
+$Report = [System.Collections.Generic.List[Object]]::new() # Create output file 
+
 try {
     Get-MsolDomain -ErrorAction Stop > $null
 }
@@ -7,12 +10,43 @@ catch {
     Connect-MsolService -Credential $cred
 }
 
-Get-MsolUser -all -Synchronized -EnabledFilter EnabledOnly | Where-Object { ($_.isLicensed -eq $true) } | 
-Sort-Object -property UserPrincipalName | 
-Select-Object UserPrincipalName, DisplayName, Department, Title, Office, 
-@{N = 'Manager'; E = { (Get-ADUser (Get-ADUser -Filter { UserPrincipalName -eq $_.UserPrincipalName } -properties *).manager).UserPrincipalName } },
-@{N = "MFA Status"; E = { if ( $_.StrongAuthenticationRequirements.State -ne $null) { $_.StrongAuthenticationRequirements.State } else { "Disabled" } } }, 
-@{n = "Methods"; e = { ($_.StrongAuthenticationMethods).MethodType } }, 
-@{n = "Method status"; e = { if ($_.StrongAuthenticationMethods.MethodType -ne $null) { "Enabled" } else { "Dislabed" } } }, 
-@{n = "Mobile"; e = { if ($_.Mobilephone -like "") { "empty" } else { "true" } } } | 
-Export-Csv c:\temp\mfa_details_not_enabled.csv -Delimiter ";" -Encoding utf8 -NoTypeInformation
+#$users = Get-MsolUser -all -EnabledFilter EnabledOnly | Where-Object { ($_.isLicensed -eq $true) } 
+
+foreach ($user in $users) {
+    $UserPrincipalName = $user.UserPrincipalName
+    $DisplayName = $user.DisplayName
+    $department = $user.department
+    $title = $user.title
+    $extensionattribute5 = (Get-ADUser -Filter { UserPrincipalName -eq $user.UserPrincipalName } -Properties Extensionattribute5).Extensionattribute5
+    $extensionattribute7 = (Get-ADUser -Filter { UserPrincipalName -eq $user.UserPrincipalName } -Properties Extensionattribute7).Extensionattribute7
+    $WhenCreated = $user.WhenCreated
+    $LastPasswordChangeTimestamp = $user.LastPasswordChangeTimestamp
+    $StsRefreshTokensValidFrom = $user.StsRefreshTokensValidFrom
+    Write-Output $UserPrincipalName
+    $MFA_default = ($user.StrongAuthenticationMethods | where { $_.isdefault -eq $true }).MethodType
+    $Methods = $user.StrongAuthenticationMethods.methodtype -join "|"
+    $Method_status = if ($user.StrongAuthenticationMethods.MethodType -ne $null) { "Enabled" } else { "Dislabed" }
+    $mfa_number = $user.StrongAuthenticationUserDetails.Phonenumber
+    $MobilePhone = if ($user.MobilePhone) {$user.MobilePhone} else { "Empty"}
+    #$Mobile = if ($user.Mobilephone -like "") { "empty" } else { "true" } 
+    $ReportLine = [PSCustomObject] @{
+        UserPrincipalName           = $UserPrincipalName
+        DisplayName                 = $DisplayName
+        Department                  = $department
+        Title                       = $title
+        Methods                     = $Methods
+        Method_status               = $Method_status
+        MFA_default                 = $MFA_default
+        extensionattribute5         = $extensionattribute5
+        extensionattribute7         = $extensionattribute7
+        WhenCreated                 = $WhenCreated
+        LastPasswordChangeTimestamp = $LastPasswordChangeTimestamp
+        StsRefreshTokensValidFrom   = $StsRefreshTokensValidFrom
+        mfa_number                  = $mfa_number
+        $MobilePhone                = $MobilePhone
+    }
+    
+    $Report.Add($ReportLine)
+} 
+$report  | Export-Csv c:\temp\mfa_details.csv -Delimiter ";" -Encoding utf8 -NoTypeInformation
+
