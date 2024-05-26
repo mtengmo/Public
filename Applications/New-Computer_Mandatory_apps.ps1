7b79cfdffd09940105d4db06218f74377964a8b7
mtengmo <magnus@highcloud.se>
2024-05-26 21:33:47 +0200

    fix more accurate timestamp for db restore

---
+++ Applications/New-Computer_Mandatory_apps.ps1
@@ -0,0 +1,10 @@
+#install the base applications needed for a new laptop
+winget install Microsoft.VisualStudioCode
+winget install git.git
+winget install google.chrome
+winget install mozilla.firefox
+winget install 9NKSQGP7F2NH #whatsapp msstore
+winget install DominikReichl.KeePass
+winget install Google.GoogleDrive
+winget install reolink.reolink
+winget install 9NCBCSZSJRSB #spotify msstore
